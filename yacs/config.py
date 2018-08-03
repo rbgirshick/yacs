178f47f4ce9a55b3a62ecb8283ae314ad262eac4
Ross Girshick <rbg@fb.com>
2018-08-03 07:21:49 -0700

    remove unwanted imports

--- yacs/config.py
+++ yacs/config.py
@@ -5,11 +5,6 @@ projects.
 See README.md for usage and examples.
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 import copy
 import io
 import logging
