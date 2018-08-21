1a24c336bb07f7d4924ee700bec54eb669a4cdf2
Ross Girshick <rbg@fb.com>
2018-08-21 13:04:23 -0700

    Update setup.py for pip

--- setup.py
+++ setup.py
@@ -2,10 +2,19 @@ from setuptools import setup
 
 setup(
     name="yacs",
-    version="0.0.1",
+    version="0.1.0",
     author="Ross Girshick",
     author_email="ross.girshick@gmail.com",
     description="Yet Another Configuration System",
+    url="https://github.com/rbgirshick/yacs",
     packages=["yacs"],
-    long_description="A simple configuration system for research",
+    long_description="A simple experiment configuration system for research",
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
 )
