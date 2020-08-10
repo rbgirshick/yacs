32d5e4ac300eca6cd3b839097dde39c4017a1070
Ross Girshick <ross.girshick@gmail.com>
2020-08-10 09:34:18 -0700

    Version bump to 0.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="yacs",
-    version="0.1.7",
+    version="0.1.8",
     author="Ross Girshick",
     author_email="ross.girshick@gmail.com",
     description="Yet Another Configuration System",
